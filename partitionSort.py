23fd875cb54d156437aa23793e925620297276a8
prushton2 <46426283+prushton2@users.noreply.github.com>
2022-12-02 14:46:40 -0500

    Add files via upload

---
+++ partitionSort.py
@@ -0,0 +1,30 @@
+#Yes i know list.extend() exists. It doesnt work on my machine. I have no clue why. I have no intention on figuring it out.
+def combineList(a, b, pivot): 
+	if(b == None or a == None):
+		return []
+	a.append(pivot)
+	for i in b:
+		a.append(i)
+	return a
+
+def partitionSort(l):
+
+	if(len(l) == 0):
+		return []
+	
+	left, right = [], []
+	pivot = l[-1]
+	l.pop(-1)
+
+	for i in l:
+		if(i < pivot):
+			left.append(i)
+		else:
+			right.append(i)
+
+	print(f'List Contents: {left}, {pivot}, {right}')
+
+	return combineList(partitionSort(left), partitionSort(right), pivot)
+
+
+print(f'List: {partitionSort([0, 2, 1, 4, 5, 3, 6, 9, 0, 8, 7])}')
